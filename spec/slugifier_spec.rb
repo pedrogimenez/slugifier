7dbd2fd1ba40ec448f8690d51be03786d5c2a4ab
Keyvan Akbary <kiwwito@gmail.com>
2015-01-14 00:32:48 +0000

    Improve char table

--- spec/slugifier_spec.rb
+++ spec/slugifier_spec.rb
@@ -8,10 +8,19 @@ describe Slugifier do
   def examples
     [
       ["Word", "word"],
+      ["JúST å fëw wørds", "just-a-few-words"],
+      ["J\"étudie le français", "j-etudie-le-francais"],
       ["An awesome slug", "an-awesome-slug"],
-      ["  should trim this text ", "should-trim-this-text"],
+      ["  should trim  this   text ", "should-trim-this-text"],
       ["Práctica de acentuación", "practica-de-acentuacion"],
-      ["Cumpleaños del muerciélago", "cumpleanos-del-muercielago"]
+      ["Cumpleaños del muerciélago", "cumpleanos-del-muercielago"],
+      ["هذا هو الاختبار", "hth-ho-l-khtb-r"],
+      ["Блоґ їжачка", "blog-jizhachka"],
+      ["Это тест", "eto-test"],
+      ["Це тест", "ce-test"],
+      ["Đây là một thử nghiệm", "day-la-mot-thu-nghiem"],
+      ["Αυτή είναι μια δοκιμή", "ayte-einai-mia-dokime"],
+      ["°¹²³@¶", "0123atp"]
     ]
   end
 end
